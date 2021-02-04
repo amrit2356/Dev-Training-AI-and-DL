ff6824730d0e02ef78618bb284e498214991912c
amrit2356 <amritanshupandey.meritocrat@gmail.com>
2021-02-04 22:41:41 +0530

    Practicing Exercises on Sigmoid Neuron

--- Exercises/01_Sigmoid_Neuron/exercise1.py
+++ Exercises/01_Sigmoid_Neuron/exercise1.py
@@ -1,94 +1,50 @@
 """
 Implementation  of Error Space of the Sigmoid Neuron
 """
-import argparse
-import matplotlib.pyplot as plt
+
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from data_prep_class import DataPreparation
+import matplotlib.pyplot as plt
 
-class Sigmoid():
-    def __init__(self):
-        self.w = None
-        self.b = None
+def sigmoid(x, w, b):
+    return 1 / (1 + np.exp(-(w * x + b)))
 
-    def perceptron(self, X):
-        return (np.dot(X, self.w.T) + self.b)
+def calc_loss(X, Y, w_est, b_est):
+    loss = 0
+    for x, y in zip(X, Y):
+        loss += (y - sigmoid(x, w_est, b_est))**2
+    return loss
 
-    def sigmoid(self, y):
-        return 1 / (1 + np.exp(-y))
+w_unknown = 0.5
+b_unknown = 0.25
 
-    def grad_w(self, y_pred, y, x):
-        return (y_pred - y) * (1 - y_pred) * (y_pred) * x
+X = np.random.random(25) * 20 - 10
+Y = sigmoid(X, w_unknown, b_unknown)
 
-    def grad_b(self, y_pred, y):
-        return (y_pred - y) * (1 - y_pred) * (y_pred) * 1
+W = np.linspace(0, 2, 100)
+B = np.linspace(-1, 1, 100)
 
-    def loss_mse(self, y, y_pred):
-        return mean_squared_error(y_pred, y)
+WW, BB = np.meshgrid(W, B)
 
-    def fit(self, X, Y, learning_rate=0.001, epochs=100, display_loss=False):
-        self.w = np.random.rand(1, X.shape[1])
-        self.b = 0
-        if display_loss:
-            loss = {}
+Loss = np.zeros(WW.shape)
 
-        for i in range(epochs):
-            dw = 0
-            db = 0
-            for x, y in zip(X, Y):
-                y_pred = self.sigmoid(self.perceptron(x))
-                dw += self.grad_w(y_pred, y, x)
-                db += self.grad_b(y_pred, y)
-            self.w -= dw * learning_rate
-            self.b -= db * learning_rate
-
-            if display_loss:
-                Y_pred = self.sigmoid(self.perceptron(X))
-                loss[i] = self.loss_mse(Y_pred, Y)
-
-        if display_loss:
-            plt.plot(loss.values())
-            plt.xlabel('Epochs')
-            plt.ylabel('Loss Value')
-            plt.show()
-
-    def predict(self, X):
-        y_pred = []
-        for x in X:
-            y = self.sigmoid(self.perceptron(x))
-            y_pred.append(y)
+for i in range(WW.shape[0]):
+    for j in range(WW.shape[1]):
+        Loss[i, j] = calc_loss(X, Y, WW[i, j], BB[i, j])
 
 
-def main(args):
-    # Creation of Dataset Object for Dataset Prep.
-    dataset_obj = DataPreparation()
-    data = dataset_obj.readcsv(args.dataset_path)
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(WW, BB, Loss, cmap='viridis')
+ax.set_xlabel('w')
+ax.set_ylabel('b')
+ax.set_zlabel('Loss')
+ax.view_init(30, 270)
+plt.show()
 
-    # Generate Binary Threshold
-    X, Y, threshold, Y_binarised = dataset_obj.generate_binary_threshold(data, 4.2)
+print(np.argmin(Loss))
 
-    # Splitting the Dataset
-    X_Train, X_Test, Y_Train, Y_Test = dataset_obj.dataset_split(X, Y, stratify=Y_binarised)
-
-    # Standardization of Dataset using StandardScaler and MinMax Scaler
-    x_sca_tn, x_sca_tes, y_sca_tn, y_sca_tes, sca_thresh = dataset_obj.standardization(
-        X_train=X_Train, X_test=X_Test, Y_train=Y_Train, Y_test=Y_Test, threshold=threshold)
-
-    # Generation of Binarized Values for Y Scaled Train and Test data.
-    Y_binarised_train, Y_binarised_test = dataset_obj.threshold_scaling(y_sca_tn, y_sca_tes, sca_thresh)
-
-    # Creation of Sigmoid Neuron Class
-    sn = Sigmoid()
-
-    # Fitting the Scaled X_train and X_test values on the Sigmoid Neuron
-    sn.fit(x_sca_tn, y_sca_tn, learning_rate=0.015, epochs=2000, display_loss=True)
-
-    y_pred_train = sn.predict(y_sca_tn)
-    y_pred_test = sn.predict(y_sca_tes)
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_path', type=str, help='Enter Dataset File Path', required=True)
-    args = parser.parse_args()
-    main(args)
+ij = np.argmin(Loss)
+i = int(np.floor(ij / Loss.shape[1]))
+j = int(ij - i * Loss.shape[1])
+print(i, j)
+print(WW[i, j], BB[i, j])
---
+++ Exercises/01_Sigmoid_Neuron/exercise3.py
@@ -0,0 +1,105 @@
+"""
+Image Classification using Sigmoid Neuron: Level 1
+"""
+import os
+import argparse
+from PIL import Image
+
+import numpy as np
+import pandas as pd
+from exercise2 import Sigmoid
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+
+def read_all(folder_path, key_prefix=""):
+    '''
+    It returns a dictionary with 'file names' as keys and 'flattened image arrays' as values.
+    '''
+    # print("Reading:")
+    images = {}
+    files = os.listdir(folder_path)
+    for i, file_name in enumerate(files):
+        file_path = os.path.join(folder_path, file_name)
+        image_index = key_prefix + file_name[:-4]
+        image = Image.open(file_path)
+        image = image.convert("L")
+        images[image_index] = np.array(image.copy()).flatten()
+        image.close()
+    return images
+
+
+def main(args):
+    LEVEL = 'level_1'
+    languages = ['ta', 'hi', 'en']
+    trainPath = args.dataset_path + LEVEL + "_train/" + LEVEL + "/"
+    testPath = args.dataset_path + LEVEL + "_test/kaggle_" + LEVEL
+    images_train = read_all(trainPath + "background", key_prefix='bkg_')
+    for language in languages:
+        images_train.update(
+            read_all(trainPath + language, key_prefix=language + "_"))
+    images_test = read_all(testPath, key_prefix='')
+    # print(len(images_train))
+    # print(len(images_test))
+
+    X_train = []
+    Y_train = []
+    for key, values in images_train.items():
+        X_train.append(values)
+        if key[:4] == 'bkg_':
+            Y_train.append(0)
+        else:
+            Y_train.append(1)
+
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train)
+
+    id_test = []
+    X_test = []
+
+    for key, values in images_test.items():
+        id_test.append(int(key))
+        X_test.append(values)
+    X_test = np.array(X_test)
+
+    scaler = StandardScaler()
+    X_scaled_train = scaler.fit_transform(X_train)
+    X_scaled_test = scaler.transform(X_test)
+    sn = Sigmoid()
+    print("Training Model...")
+    sn.fit(X_scaled_train, Y_train, learning_rate=args.learning_rate,
+           epochs=args.epochs, loss_type=args.loss_type, display_loss=args.display_loss)
+
+    Y_pred_train = sn.predict(X_scaled_train)
+    Y_pred_test = sn.predict(X_scaled_test)
+    threshold = 0.5
+    Y_pred_binarised_train = (Y_pred_train >= threshold).astype("int").ravel()
+    Y_pred_binarised_test = (Y_pred_test >= threshold).astype("int").ravel()
+    accuracy_train = accuracy_score(Y_pred_binarised_train, Y_train)
+    print("Train Accuracy : {:.3f} ".format(accuracy_train))
+
+    Y_pred_test = sn.predict(X_scaled_test)
+    Y_pred_binarised_test = (Y_pred_test >= 0.5).astype("int").ravel()
+
+    submission = {}
+    submission['ImageId'] = id_test
+    submission['Class'] = Y_pred_binarised_test
+
+    submission = pd.DataFrame(submission)
+    submission = submission[['ImageId', 'Class']]
+    submission = submission.sort_values(['ImageId'])
+    submission.to_csv("submission_{}.csv".format(LEVEL), index=False)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_path', type=str,
+                        help='Enter Dataset File Path', required=True)
+    parser.add_argument('--learning_rate', type=float,
+                        help='Enter the Learning Rate[0, 1]')
+    parser.add_argument('--epochs', type=int,
+                        help='Enter the Number of Epochs')
+    parser.add_argument('--loss_type', type=str,
+                        help='Choose Loss Type: MSE or CE')
+    parser.add_argument('--display_loss', type=bool,
+                        help='Choose Loss Type: MSE or CE')
+    args = parser.parse_args()
+    main(args)
